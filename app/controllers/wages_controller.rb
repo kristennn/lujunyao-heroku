ac4b8023ec2aee6c748584c71b1755264379542c
13949836783@163.com <13949836783@163.com>
2019-02-22 16:42:07 +0800

    工资部分结构完成

--- app/controllers/wages_controller.rb
+++ app/controllers/wages_controller.rb
@@ -1,6 +1,10 @@
 class WagesController < ApplicationController
   layout 'home'
   def index
-    
+    @employees = Employee.all
+  end
+
+  def show
+    @wage = Wage.find(params[:id])
   end
 end
