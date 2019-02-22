d711dd6d4d9bcd1eb1ebe08ff3a05388097339b2
13949836783@163.com <13949836783@163.com>
2019-02-22 18:48:24 +0800

    交易记录部分架构完成

--- app/controllers/trading_records_controller.rb
+++ app/controllers/trading_records_controller.rb
@@ -1,5 +1,13 @@
 class TradingRecordsController < ApplicationController
   layout 'home'
+
+  def index
+    @trading_records = TradingRecord.all
+  end
+
   def new
+    @trading_record = TradingRecord.new
+    @commodities = Commodity.all
+    @employees = Employee.all
   end
 end
